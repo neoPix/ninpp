817180a2ca2ea50d629193cb0c765d4a231ce2b4
neoPix <neoPix@users.noreply.github.com>
2014-11-04 22:42:11 +0100

    Gestion des uploads et préparation du serveur.

--- Ortcoi/server.js
+++ Ortcoi/server.js
@@ -1,5 +1,7 @@
 var multiparty = require('multiparty')
-  , http = require('http');
+  , http = require('http')
+  , fs = require('fs')
+  , jszip = require('jszip');
 
 var Queue = function(){ this._queue = new Array(); };
 Queue.prototype = {
@@ -17,16 +19,26 @@ Queue.prototype = {
 
 var App = function(port){ 
     this._server = null;
+    this._curent = null;
     this._queue = new Queue();
+    this._done = new Array();
     this.initialize(port);
  }
 App.prototype = {
     _server: null,
+    _curent: null,
+    _queue: null,
+    _done: null,
     initialize : function(port){
         port = port || 8080;
         var $this = this;
 
         this._server = http.createServer(function(request, response) {
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            if (request.method == 'OPTIONS') {
+                response.send(200);
+                return;
+            }
             switch(request.url){
                 case '/Upload' :
                     $this.manageUpload(request, response);
@@ -57,12 +69,64 @@ App.prototype = {
             $this._queue.enqueue(element);
 
             var result = JSON.stringify({token : element.token, state: element.state, position: $this._queue.size()}, null, 3);
-            response.setHeader('Access-Control-Allow-Origin', '*');
             response.setHeader('Content-Type', 'application/json');
             response.end(result);
+
+            $this.manageQueue();
         });
 
         form.parse(request);
+    },
+    manageQueue: function(){
+        var $this = this;
+        if(this._current == null && this._queue.size() > 0){
+            this._current = this._queue.dequeue();
+            this.copyFile(this._current, function(){
+                $this.openZip();
+            }, function(message){
+                $this.manageError(message);
+            });
+            
+        }
+    },
+    openZip: function(){
+        var $this = this;
+        fs.readFile('./curent.zip', function(err, data) {
+            if (err) {
+                $this.manageError(err);
+                return;
+            }
+            var zip = new jszip(data);
+            $this.manageZip(zip);
+        });
+    },
+    manageZip: function(zip){
+        // Convertion audio et video
+        // Génération des miniatures image
+        // Préparation du HTML
+        // Regroupement dans un fichier zip /done/{code}.zip
+        // Passage en done -> téléchargement client
+
+        this._current.state = 'done';
+        console.log(this._current);
+        this._done.push(this._current);
+        this._current = null;
+        this.manageQueue();
+    },
+    manageError: function(error){
+        this._current.error = error;
+        this._current.state = 'failed';
+        console.log(this._current);
+        this._done.push(this._current);
+        this._current = null;
+        this.manageQueue();
+    },
+    copyFile: function(element, end, error){
+        var source = fs.createReadStream(element.file.path);
+        var dest = fs.createWriteStream('./curent.zip');
+        source.pipe(dest);
+        source.on('end', end);
+        source.on('error', error);
     }
 };
 var app = new App(8080);
